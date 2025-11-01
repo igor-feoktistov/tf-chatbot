7e4ac8451951855cdbac43dd8c75e32f79aa1ba4
Igor Feoktistov <igorf@netapp.com>
2025-10-31 18:05:12 -0700

    tf-chatbot v1.0.10

--- cmd/main.go
+++ cmd/main.go
@@ -10,10 +10,10 @@ import (
 func main() {
 	var chatBot *chatbot.ChatBot
 	var err error
-        optConfig := flag.String("configPath", "/usr/local/etc/chatbotConfig.yaml", "ChatBot configuration path")
-        optStaticPath := flag.String("staticPath", "/usr/local/etc/html", "ChatBot static HTML directory path")
-        flag.Parse()
-        if chatBot, err = chatbot.ChatBotInitialize(*optConfig, *optStaticPath); err != nil {
+	optConfig := flag.String("configPath", "/usr/local/etc/chatbotConfig.yaml", "ChatBot configuration path")
+	optStaticPath := flag.String("staticPath", "/usr/local/etc/html", "ChatBot static HTML directory path")
+	flag.Parse()
+	if chatBot, err = chatbot.ChatBotInitialize(*optConfig, *optStaticPath); err != nil {
 		log.Fatalf("failure to initialize ChatBot: %s", err)
 	}
 	chatBot.Run()
